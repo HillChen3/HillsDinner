d04a066f85bbbc03221433e543fe4997b1876d72
DeronLee <akagilnc@gmail.com>
2018-04-29 18:28:20 +0900

    add group user verify apis

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restplus import Api
-from resource import Account, User, CommGroup, GroupUserLink
+from resource import Account, User, CommGroup, GroupUserLink, GroupUserVerify
 
 
 app = Flask(__name__)
@@ -13,6 +13,8 @@ api.add_resource(User.User, '/user/<user_id>')
 api.add_resource(User.UserList, '/user/<group_id>')
 api.add_resource(CommGroup.Comm_groups, '/comm_group')
 api.add_resource(GroupUserLink.Group_User_link, '/group_user_link')
+api.add_resource(GroupUserVerify.Group_User_Verify, '/group_user_verify/<verify_id>')
+api.add_resource(GroupUserVerify.Group_User_Verify_List, '/group_user_verify/<group_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
