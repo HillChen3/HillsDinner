b996e4f7d7d810f4d122016b929f340a366b80da
DeronLee <akagilnc@gmail.com>
2018-05-13 16:16:30 +0900

    get all and post function done

--- apis/user.py
+++ apis/user.py
@@ -14,6 +14,9 @@ operation_model_reg = api.model('OperationModel', operation_model)
 group_model_reg = api.model('GroupModel', group_model)
 group_user_verify_model_reg = api.model('VerifyModel', group_user_verify_model)
 
+query_user = 'SELECT id, username, nickname, avatar, gender, ' \
+                     'phone_num, job, wechat_id, constellation, pet_plant, ' \
+                     'hobbies, fav_event_type, self_intro FROM users'
 
 def abort_if_todo_doesnt_exist(api_id):
     if api_id not in APIS:
@@ -30,7 +33,6 @@ def set_in_progress_model(args):
 class UserList(Resource):
     @api.marshal_list_with(user_model_reg)
     def get(self):
-        query_user = 'SELECT id, username, nickname, avatar, gender FROM users'
         result = db_utils.query(query_user)
         if not result:
             return 'no content', 204
@@ -41,13 +43,21 @@ class UserList(Resource):
     @api.doc(body=user_model_reg)
     def post(self):
         add_user = ('INSERT INTO users '
-                    '(username, nickname, avatar, gender) values ("{}", "{}", "{}", "{}")')
+                    '(username, nickname, avatar, gender, '
+                    'phone_num, job, wechat_id, constellation, pet_plant, '
+                    'hobbies, fav_event_type, self_intro ) values ('
+                    '"{}", "{}", "{}", {}, "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}")')
 
         for key, value in user_model.items():
             parser.add_argument(key, type=str, required=True)
         args = parser.parse_args()
         print(args)
-        add_user = add_user.format(args['username'], args['nickname'], args['avatar'], args['gender'])
+        # for item in args:
+        #     add_user += '"' + item + '", '
+        add_user = add_user.format(
+            args['username'], args['nickname'], args['avatar'], args['gender'],
+            args['phone_num'], args['job'], args['wechat_id'], args['constellation'], args['pet_plant'],
+            args['hobbies'], args['fav_event_type'], args['self_intro'])
         # 需要检查电话号码格式
         if utils.check_phone_num(args['phone_num']):
             db_utils.no_query(add_user)
@@ -60,8 +70,8 @@ class UserList(Resource):
 class User(Resource):
     @api.marshal_with(user_model_reg)
     def get(self, user_id):
-        query_user = "SELECT id, username, nickname, avatar, gender FROM users WHERE id = {}".format(user_id)
-        result = db_utils.query(query_user)
+        query_single_user = query_user + "WHERE id = {}".format(user_id)
+        result = db_utils.query(query_single_user)
         if result:
             response = db_utils.make_dict_by_model(value=result[0], model=user_model)
             print(response)
@@ -81,7 +91,11 @@ class User(Resource):
             parser.add_argument(key, type=str, required=True)
         args = parser.parse_args()
         print(args)
-        update_user = update_user.format(args['username'], args['nickname'], args['avatar'], args['gender'], user_id)
+        update_user = update_user.format(
+            args['username'], args['nickname'], args['avatar'], args['gender'],
+            args['phone_num'], args['job'], args['wechat_id'], args['constellation'], args['pet_plant'],
+            args['hobbies'], args['fav_event_type'], args['self_intro'],
+            user_id)
         # 需要检查电话号码格式
         if utils.check_phone_num(args['phone_num']):
             db_utils.no_query(update_user)
